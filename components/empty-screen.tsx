2011a52f986645fc775ab972f56c6574792b73f3
thepartisan101 <9511307+thepartisan101@users.noreply.github.com>
2023-07-14 16:31:27 +0300

    Add initial files

---
+++ components/empty-screen.tsx
@@ -0,0 +1,56 @@
+import { UseChatHelpers } from 'ai/react'
+
+import { Button } from '@/components/ui/button'
+import { ExternalLink } from '@/components/external-link'
+import { IconArrowRight } from '@/components/ui/icons'
+
+const exampleMessages = [
+  {
+    heading: 'Explain what is civic lottery',
+    message: `What is the concept of "civic lottery"?`
+  },
+  {
+    heading: 'Analize a headline&apos;s objectivity',
+    message: 'Please analyze the following headline by measuring the subjectivity of the language used, whether the message is biased, the sentiment of the message, an also rewrite the headline in a neutral tone if needed: \n'
+  },
+  {
+    heading: 'Explain how a legislative drafting is done',
+    message: `Explain they key points and steps of drafting a legislative bill`
+  }
+]
+
+export function EmptyScreen({ setInput }: Pick<UseChatHelpers, 'setInput'>) {
+  return (
+    <div className="mx-auto max-w-2xl px-4">
+      <div className="rounded-lg border bg-background p-8">
+        <h1 className="mb-2 text-lg font-semibold">
+          Welcome to Deliberativa&apos;s Chatbot!
+        </h1>
+        <p className="mb-2 leading-normal text-muted-foreground">
+          An open source AI chatbot app template built with{' '}
+          <ExternalLink href="https://nextjs.org">Next.js</ExternalLink> and{' '}
+          <ExternalLink href="https://vercel.com/storage/kv">
+            Vercel KV
+          </ExternalLink>
+          .
+        </p>
+        <p className="leading-normal text-muted-foreground">
+          You can start a conversation here or try the following:
+        </p>
+        <div className="mt-4 flex flex-col items-start space-y-2">
+          {exampleMessages.map((message, index) => (
+            <Button
+              key={index}
+              variant="link"
+              className="h-auto p-0 text-base"
+              onClick={() => setInput(message.message)}
+            >
+              <IconArrowRight className="mr-2 text-muted-foreground" />
+              {message.heading}
+            </Button>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
